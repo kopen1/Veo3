d33ad27ca89e32826c435498e70b43c504f15894
Kopen <89531508+kopen1@users.noreply.github.com>
2025-09-20 13:46:28 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// ⚠️ Ganti "username" & "repo-name" sesuai GitHub kamu
 export default defineConfig({
   plugins: [react()],
-  base: '/Veo3/', 
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
-})
+  base: "./",
+});
